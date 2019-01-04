9d28efc304a10156d0648e6269d63a7149d5f007
David Xiedeng <dxiedeng00@stuy.edu>
2019-01-04 14:47:16 -0500

    working on Sheet constructor

--- Sheet.java
+++ Sheet.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Sheet {
   private ArrayList<ArrayList<Cell>> data;
   private int[] rows;
   private int[] cols;
+
+  public Sheet(String filename) throws FileNotFoundException {
+    File csv = new File(filename);
+    Scanner in = new Scanner(csv);
+    while(in.hasNext()) {
+      
+    }
+  }
 }
