96d563109c7bbe1c585f0c2d8294d43982c9cba0
Hilary Zen <hzen00@stuy.edu>
2019-01-15 14:33:51 -0500

    Commented out lines

--- Driver.java
+++ Driver.java
@@ -38,9 +38,9 @@ public class Driver {
     System.out.println("Longest entry in column 2: " + sheet1.longestInCol(2));
     */
 
-    System.out.println(sheet1.get());
-    System.out.println(sheet1.getCell(100, 200));
-    System.out.println(sheet1);
+    //System.out.println(sheet1.get());
+    //System.out.println(sheet1.getCell(100, 200));
+    //System.out.println(sheet1);
 
 		//testing if Integers are stored seperate from Strings
 		//System.out.println(sheet1.getCell(2,2));
@@ -63,7 +63,7 @@ public class Driver {
     System.out.println(sheet1.get());
     System.out.println("Adding cell in row 1 col 3: " + sheet1.select(1,3));
     System.out.println(sheet1.get());
-		
+
     System.out.println(" ");
 
     String[] newRow1 = new String[] {"Ann", "444555", "100", "1958", "6", "70"};
