9f7a859f26cf55504f8795472324d8e0c910f734
David Xiedeng <dxiedeng00@stuy.edu>
2019-01-20 11:07:25 -0500

    sortRow: was y++ instead of y--

--- Sheet.java
+++ Sheet.java
@@ -349,7 +349,7 @@ public class Sheet {
   public void sortRow(int col) {
     for (int x = 2; x < rows(); x++) { //ignore first row
       String value = getString(x, col);
-      for (int y = x; y > 1; y++) {// move back down stop before first row
+      for (int y = x; y > 1; y--) {// move back down stop before first row
         if (value.compareTo(getString(y - 1,col)) == -1) { //compare row above
           data.add(y - 1, data.get(x)); //bumps up the size 1
           data.remove(x + 1); 
